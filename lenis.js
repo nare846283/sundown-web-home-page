9dd2c3bf10f6704294ed5850b6059505490bb7bf
Narendra Ahirwar <narendraahirwar37@gmail.com>
2024-01-26 09:04:30 +0530

    sundown home page

---
+++ lenis.js
@@ -0,0 +1,44 @@
+"use strict";
+        // fix lenis in safari
+
+        if (Webflow.env("editor") === undefined) {
+            const lenis = new Lenis({
+                lerp: 0.1,
+                wheelMultiplier: 0.7,
+                infinite: false,
+                gestureOrientation: "vertical",
+                normalizeWheel: false,
+                smoothTouch: false
+            });
+
+            function raf(time) {
+                lenis.raf(time);
+                requestAnimationFrame(raf);
+            }
+            requestAnimationFrame(raf);
+
+            $("[data-lenis-start]").on("click", function () {
+                lenis.start();
+            });
+            $("[data-lenis-stop]").on("click", function () {
+                lenis.stop();
+            });
+            $("[data-lenis-toggle]").on("click", function () {
+                $(this).toggleClass("stop-scroll");
+                if ($(this).hasClass("stop-scroll")) {
+                    lenis.stop();
+                } else {
+                    lenis.start();
+                }
+            });
+
+            function connectToScrollTrigger() {
+                lenis.on("scroll", ScrollTrigger.update);
+                gsap.ticker.add((time) => {
+                    lenis.raf(time * 1000);
+                }
+                );
+            }
+            // Uncomment this if using GSAP ScrollTrigger
+            // connectToScrollTrigger();
+        }
